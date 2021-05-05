165af23b1935ad20885703039b7eb522eecb4cb7
Heather Stratton <hastratton@gmail.com>
2021-05-04 21:49:30 -0400

    added ta03

--- routes/teamRoutes/ta03.js
+++ routes/teamRoutes/ta03.js
@@ -1,29 +1,51 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
-/*
-var jsonEngine = require('../../controller/w3);
+const url = "https://byui-cse.github.io/cse341-course/lesson03/items.json";
+const fetch = require('node-fetch');
+//var jsonEngine = require('../../controllers/wk3');
+let items = [];
 
-router.get('/', jsonEngine.processJson
-    .post('/', (req, res, next) => {
-        let searchedValue = req.body.search ?;
-        let filterData = global.jsonResponse ?;
-        console.log(filteredData);
-        res.render('pages/teamActivities/ta03', {
-            title: 'JSON search',
-            data: filterData,
-            path: '/ta03', // For pug, EJS 
-            searchedValue: searchedValue
-        });
+//const callFetch = (url) => {
+fetch(url)
+    .then(function (response) {
+        return response.json();
     })
-*/
+    .then(function (jsonObject) {
+        //console.log(jsonObject);
+
+        // const itms = jsonObject;
+        // for (let i = 0; i < jsonObject.length; i++) {
+        //console.log(jsonObject[i].name);
+        console.log('jsonObject is real');
+        // }
+
+        items = jsonObject;
+    })
+    .catch(err => console.log(err));
+    
+//  };
+
 router.get('/', (req, res, next) => {
-        res.render('pages/teamActivities/ta03', {
-            title: 'Team Activity 03',
-            path: '/ta03', // For pug, EJS 
-            //activeTA03: true, // For HBS
-            //contentCSS: true, // For HBS
-        });
+    res.render('pages/teamActivities/ta03', {
+        title: 'Team Activity 03',
+        path: '/ta03',
+        items: items// For pug, EJS 
+        //activeTA03: true, // For HBS
+        //contentCSS: true, // For HBS
     });
+})
+router.post('/', (req, res, next) => {
+    const searchWord = req.body.input1;
+    console.log(searchWord);
+    const found = items.tags.filter(word => word === searchWord);
+    console.log(found);
+
+    res.render('./', {
+        title: "filtered",
+        path: '/ta03',
+        items:found
+    });
+})
 
 module.exports = router;
