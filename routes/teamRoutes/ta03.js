105a1599db202a668a79e8d184ebd3c1403a3ad6
Heather Stratton <hastratton@gmail.com>
2021-05-07 13:37:07 -0400

    to03

--- routes/teamRoutes/ta03.js
+++ routes/teamRoutes/ta03.js
@@ -3,45 +3,42 @@ const express = require('express');
 const router = express.Router();
 const url = "https://byui-cse.github.io/cse341-course/lesson03/items.json";
 const fetch = require('node-fetch');
-//var jsonEngine = require('../../controllers/wk3');
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
 let items = [];
 
-//const callFetch = (url) => {
 fetch(url)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
-        //console.log(jsonObject);
-
-        // const itms = jsonObject;
-        // for (let i = 0; i < jsonObject.length; i++) {
-        //console.log(jsonObject[i].name);
         console.log('jsonObject is real');
-        // }
-
         items = jsonObject;
     })
     .catch(err => console.log(err));
     
-//  };
 
 router.get('/', (req, res, next) => {
     res.render('pages/teamActivities/ta03', {
         title: 'Team Activity 03',
         path: '/ta03',
-        items: items// For pug, EJS 
-        //activeTA03: true, // For HBS
-        //contentCSS: true, // For HBS
+        items: items
     });
 })
-router.post('/', (req, res, next) => {
+router.post('/search', (req, res, next) => {
     const searchWord = req.body.input1;
-    console.log(searchWord);
-    const found = items.tags.filter(word => word === searchWord);
-    console.log(found);
+    //console.log(searchWord);
+    let searchString = searchWord.toString();
+    //console.log(searchString);
+    let lowerSearch = searchString.toLowerCase();
+    //console.log(lowerSearch);
+    let upperSearch = lowerSearch[0].toUpperCase() + lowerSearch.substring(1);
+    //console.log(upperSearch);
+    //console.log(items[0]);
+    let found = items.filter((word) =>
+        word.tags.includes(upperSearch));
 
-    res.render('./', {
+    res.render('pages/teamActivities/ta03', {
         title: "filtered",
         path: '/ta03',
         items:found
