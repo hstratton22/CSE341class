0be6c56063baca048ba9957c07b90a629356256c
Heather Stratton <hastratton@gmail.com>
2021-05-15 15:55:50 -0400

    comment console.log

--- routes/proveRoutes/prove04/controllers/auth.js
+++ routes/proveRoutes/prove04/controllers/auth.js
@@ -2,17 +2,17 @@ const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
     //req.isLoggedin = true;
-    console.log(req.get('Cookie'));
-    const isLoggedIn = req
-        .get('Cookie')
-        .split(';')[0]
-        .trim()
-        .split('=')[1] === 'true';
-    console.log(req.session.isLoggedIn);
+    // console.log(req.get('Cookie'));
+    // const isLoggedIn = req
+    //     .get('Cookie')
+    //     .split(';')[0]
+    //     .trim()
+    //     .split('=')[1] === 'true';
+    // console.log(req.session.isLoggedIn);
     res.render('pages/proveAssignments/prove04/auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn//false
+        isAuthenticated: false//isLoggedIn//false
     });
 }
 exports.postLogin = (req, res, next) => {
